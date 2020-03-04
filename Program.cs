b427dffe9fab3cf46c91fdc3a168016670c86312
prasadacharya <prasada@mail.usf.edu>
2020-03-03 23:35:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SmallerThanCurrent
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 8, 1, 2, 2, 3 };
+            SmallerThanCurrent(nums);
+        }
+        static void SmallerThanCurrent(int[] N)
+        {
+            int[] smallarr = new int[N.Length];
+
+            int k = 0;
+            for (int i = 0; i < N.Length; i++)
+
+            {
+                int count = 0;
+                for (int j = 0; j < N.Length; j++)
+                    if (N[i] != N[j])
+                    {
+                        if (N[i] > N[j])
+                            count++;
+                    }
+
+                smallarr[k] = count;
+                k++;
+
+            }
+
+            foreach (int i in smallarr)
+            {
+                Console.Write(i);
+            }
+
+        }
+    }
+}
+
+
+
